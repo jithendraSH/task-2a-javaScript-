3e7b26193d1fcda899c946b7175a571538a824c6
BruceBanner313 <jithendraarimilli@gmail.com>
2022-08-20 13:22:05 +0530

    while add

---
+++ fact with while loop.js
@@ -0,0 +1,11 @@
+function fact2(num) {
+    var result = num;
+    if (num === 0 || num === 1) 
+      return 1; 
+    while (num > 1) { 
+      num--;
+      result *= num;
+    }
+    return result;
+  }
+  console.log(fact2(7));
