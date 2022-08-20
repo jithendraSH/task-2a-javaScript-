ee9ef2693017fab63a9734769b010c369e5a28f1
BruceBanner313 <jithendraarimilli@gmail.com>
2022-08-20 13:05:03 +0530

    factoral

---
+++ factorial with for loop.js
@@ -0,0 +1,9 @@
+function fact1(num) {
+    if (num === 0 || num === 1)
+      return 1;
+    for (var i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+ console.log(fact1(6));
